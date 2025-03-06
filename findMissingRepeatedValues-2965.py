281dcedcb42f5b399ae322fca7ca04c4a661a655
colebui <colebui@gmail.com>
2025-03-06 17:48:48 -0600

    faster n2 time of find missing rpeated values

--- findMissingRepeatedValues-2965.py
+++ findMissingRepeatedValues-2965.py
@@ -1,20 +1,41 @@
 class Solution:
     def findMissingAndRepeatedValues(self, grid: List[List[int]]) -> List[int]:
-        highestNumber = len(grid) ** 2
-        a = None
-        b = None
+        
+        freq = {}
 
-        numbersList = []
-        for i in range(1, highestNumber + 1):
-            numbersList.append(i)
+        for row in grid:
+            for num in row:
+                freq[num] = freq.get(num, 0) + 1
 
-        print(numbersList)
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                try:
-                    numbersList.remove(grid[i][j])
-                except ValueError:
-                    a = grid[i][j]
+        for i in range(1, len(grid) ** 2 + 1):
+            if (i not in freq):
+                b = i
+            elif (freq[i] == 2):
+                a = i
 
-        b = numbersList[0]
         return [a,b]
+
+
+
+
+
+# class Solution:
+#     def findMissingAndRepeatedValues(self, grid: List[List[int]]) -> List[int]:
+#         highestNumber = len(grid) ** 2
+#         a = None
+#         b = None
+
+#         numbersList = []
+#         for i in range(1, highestNumber + 1):
+#             numbersList.append(i)
+
+#         print(numbersList)
+#         for i in range(len(grid)):
+#             for j in range(len(grid[0])):
+#                 try:
+#                     numbersList.remove(grid[i][j])
+#                 except ValueError:
+#                     a = grid[i][j]
+
+#         b = numbersList[0]
+#         return [a,b]
