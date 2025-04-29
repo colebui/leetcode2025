a12e569b08b8a0c6fd04710710f529f714e18688
colebui <colebui@gmail.com>
2025-04-28 23:40:13 -0500

    added second solution

--- addBinary67.py
+++ addBinary67.py
@@ -4,3 +4,23 @@ class Solution:
         num2 = int(b, 2)   
         total = num1 + num2
         return bin(total)[2:] 
+
+
+    def addBinary2(a, b):
+        result = []
+        i, j = len(a) - 1, len(b) - 1
+        carry = 0
+        
+        while i >= 0 or j >= 0 or carry:
+            total = carry
+            if i >= 0:
+                total += int(a[i])
+                i -= 1
+            if j >= 0:
+                total += int(b[j])
+                j -= 1
+            
+            result.append(str(total % 2))
+            carry = total // 2
+
+        return ''.join(reversed(result))
